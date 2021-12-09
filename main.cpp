6e176041b23d10535128331f386b300d9d458520
Vladimir <varzaru979@gmail.com>
2021-12-09 09:56:38 +0200

    Added one more function

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@ void func(int par1)
     cout << "I am function with parameter " << par1;
 }
 
+void another_func()
+{
+    cout << "Print some text";
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
